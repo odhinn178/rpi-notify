d41150bce6e549937dfc387c25f8091ff7799904
Scott Wohler <scott.wohler@link-labs.com>
2016-06-11 17:09:55 -0400

    Using Bootstrap for better looking pages.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from app import config
 from app import notify
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
+Bootstrap(app)
 
 rpi_notify = notify.RPiNotify(config.app_cfg, config.cert_path)
 
