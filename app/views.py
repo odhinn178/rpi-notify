05240301da20fc14f6ba86f68141c3a68f2234de
Scott Wohler <scott.wohler@link-labs.com>
2016-06-11 17:10:20 -0400

    Sending MQTT messages to AWS IoT

--- app/views.py
+++ app/views.py
@@ -7,10 +7,12 @@ from app.forms import NotifyForm
 def index():
     form = NotifyForm()
     if form.validate_on_submit():
-        rpi_notify.send_notification(form.message, form.priority)
+        msg = form.message.data
+        priority = form.priority.data
+        rpi_notify.send_notification(msg, priority)
         flash('Message sent')
         return redirect(url_for('index'))
-    return render_template('index,html', form=form)
+    return render_template('index.html', form=form)
 
 
 app.add_url_rule('/', 'index', index, methods=['GET', 'POST'])
